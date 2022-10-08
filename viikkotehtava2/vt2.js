512933abfc115b7470004ce46aee78356c1115a2
Kimi Haukka <haukka.kimi@gmail.com>
2022-10-08 03:11:54 +0300

    Added functions joukkue and korvaaJoukkue.

--- viikkotehtava2/vt2.js
+++ viikkotehtava2/vt2.js
@@ -19,13 +19,67 @@ function start(data) {
 	// tästä eteenpäin omaa koodia
         // vinkki: muunna data ensimmäisen viikkotehtävän tyyppiseksi rakenteeksi
         // jolloin saat siitä helpommin ja tehokkaammin käsiteltävää
-	console.log(data);
         // dataa voi tutkia myös osoitteesta: https://appro.mit.jyu.fi/cgi-bin/tiea2120/randomize.cgi
         // huom. datan sisältö muuttuu hieman jokaisella latauskerralla
+
+	console.log(data);
+
 	console.log(data.documentElement);
 	console.log(data.documentElement.getElementsByTagName("joukkue"));
+        console.log(data.documentElement.getElementsByTagName("sarja"));
+
+        //console.log(getSarjaById("6995217"));
+
+        let p = document.createElement('p');
+        p.textContent = "tesingss11";
+        let otsikko = document.documentElement.getElementsByTagName('h1');
+        otsikko[0].appendChild(p);
+
+        joukkue();
+ 
         // savedata tallentaa datan selaimen LocalStorageen. Tee tämä aina, kun
         // ohjelmasi on muuttanut dataa. Seuraavalla sivun latauskerralla
         // saat käyttöösi muuttamasi datan
 	savedata(data);
+
+/**
+ * Luodaan taulukko joukkeista.
+ * 
+ */
+function joukkue(){
+        let joukkueet = data.documentElement.getElementsByTagName("joukkue");
+        let joukkueTaulukko = [];
+        for (let joukkue of joukkueet) {
+                let jou = {
+                "aika": joukkue.getAttribute("aika"),
+                "matka": joukkue.getAttribute("matka"),
+                "pisteet": joukkue.getAttribute("pisteet"),
+                "sarja": joukkue.getAttribute("sarja"),
+                "nimi": joukkue.getElementsByTagName("nimi")
+        };
+        joukkueTaulukko.push(jou);
+                
+}
+        console.log(JSON.parse(JSON.stringify("omaT: "+joukkueTaulukko)));
+}
+
+
+}
+
+
+
+function korvaaJoukkue(){
+        let joukkueet = data.documentElement.getElementsByTagName("joukkue");
+        let jnimi;
+        for(let joukkue of joukkueet){
+                if(joukkue.childNodes.nimi[""] != null){
+                        jnimi = joukkue.childNodes.nimi;
+                        console.log(jnimi);
+                }
+              //  let replace = joukkueet.getElementById("Joukkue 1");
+              //  replace.parentNode.replaceChild(document.createTextNode(jnimi), replace);
+             //   th.parentNode.replaceChild(document.createTextNode(jnimi), th);
+        }
+
+
 }
