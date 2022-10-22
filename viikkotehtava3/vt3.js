568ce9592b3381c83d969f408f83e557d6cf25aa
Kimi Haukka <haukka.kimi@gmail.com>
2022-10-22 20:08:35 +0300

    Added functions lisaaCheckBox, uusiJoukkue, joukkueet and eventlistener for tallenna button.

--- viikkotehtava3/vt3.js
+++ viikkotehtava3/vt3.js
@@ -48,6 +48,20 @@ function alustus() {
 
 // oma sovelluskoodi voidaan sijoittaa tähän funktioon
 function start(data) {
+
+
+ // Kutsutaan funktioita:
+ joukkueet(); 
+ lisaaCheckBox();
+
+ // Luodaan globaalit taulukot:
+ //let joukkueTaulukko = [];
+
+ // Olemassa olevat sarjat: ESIM:
+ // 4h, 2h, 8h, 6 tuntia, pitkä sarja 14 ja pitkä sarja 11
+
+
+
   // tänne oma koodi
   console.log(data);
   // tallenna data sen mahdollisten muutosten jälkeen aina localStorageen. 
@@ -58,6 +72,143 @@ function start(data) {
   // resetoi rakenne tarvittaessa lisäämällä sivun osoitteen perään ?reset=1
   // esim. http://users.jyu.fi/~omatunnus/TIEA2120/vt2/pohja.xhtml?reset=1
 
+
+
+/**
+ * Funktiolla lisaaCheckBox luodaan checkbox -ja niiden label -elementit.
+ */
+function lisaaCheckBox(){
+
+  // Etsii div elementint id:n perusteella.
+  let boxit = document.getElementById("boxes");
+
+  let sarjat = data.sarjat;
+
+  // Käydään datan sarjat läpi ja luodaan checkboxit + labelit.
+  for(let sarja of sarjat){
+
+    // Luodaan label ja tarvittavat attribuutit.
+    let label = document.createElement("label");
+    label.className = "oikea";
+    label.appendChild(document.createTextNode(sarja.nimi));
+    
+    boxit.appendChild(label);
+
+    // Luodaan checkboxit ja tarvittavat attribuutit.
+    let cbox = document.createElement("input");
+    cbox.type = "radio";
+    cbox.name = "rg";
+    cbox.id = sarja.nimi;
+    cbox.checked = true; // Tällöin viimeinen on aina checked.
+
+    label.appendChild(cbox);
+
+    // Luodaan väli.
+    let breik = document.createElement("br");
+    boxit.appendChild(breik);
+  }
 }
+
+
+
+
+
+// Valitsee ensimmäisen form -elementin.
+let form = document.forms["lomake"];
+
+/**
+ * Asetetaan lisaa napin toimivuus, mahdollistetaan rastin lisääminen
+ * tietokantaan.
+ */
+ form.addEventListener("submit", function (e) {
+               
+  e.preventDefault(); // estetään lomakkeen lähettäminen
+
+  // Etsitään nimi -input elementin sijainti.
+  let jnimi = document.forms.lomake[1];
+  console.log("Joukkueen nimi: ", jnimi.value);
+
+  
+  let sarjanId;
+  // Etsitään valittu checkbox ja tämän sarjan id.
+  for (let sarja of data.sarjat){
+     if(document.getElementById(sarja.nimi).checked == true){
+      sarjanId = sarja.id;
+     }
+  }
+  console.log("Joukkueen sarjan id: ", sarjanId);
+
+  // Etsitään jäsen 1 -input elementin sijainti.
+  let jasen1 = document.getElementById("jasen1");
+  console.log("Joukkueen jasen 1: ", jasen1.value);
+ 
+  // Etsitään jäsen 2 -input elementin sijainti.
+  let jasen2 = document.getElementById("jasen2");
+  console.log("Joukkueen jasen 2: ", jasen2.value);
+
+  uusiJoukkue(jnimi.value,sarjanId,jasen1.value,jasen2.value);
+  localStorage.setItem("TIEA2120-vt3-2022s", JSON.stringify(data)); // Tallennetaan localstorageen.
+
+  console.log(data.joukkueet);
+
+  form.reset();   
+  
+});
+
+/**
+ * Luodaan uusi joukkue.
+ * 
+ * @param {*} nimi 
+ * @param {*} sarja 
+ * @param {*} jasen1 
+ * @param {*} jasen2 
+ */
+function uusiJoukkue(nimi,sarja,jasen1,jasen2){
+
+  let newTeam = {
+    aika: "00:00:00",
+    jasenet: [jasen1,jasen2],
+    leimaustapa:["0"],
+    matka: 0,
+    nimi: nimi,
+    pisteet: 0,
+    rastileimaukset: [],
+    sarja: sarja,
+    
+  };
+  data.joukkueet.push(newTeam);
+}
+
+
+
+/**
+ * Funktiolla luodaan deepcopy taulukko alkuperäisestä data.joukkueet taulukosta.
+ */
+ function joukkueet(){
+
+  let joukkueTaulukko = [];
+
+  let joukkueet = data.joukkueet;
+  for (let joukkue of joukkueet) {
+          let joukkueAttr = {
+          aika: joukkue.aika,
+          jasenet: joukkue.jasenet,
+          leimaustapa: joukkue.leimaustapa,
+          matka: joukkue.matka,
+          nimi: joukkue.nimi,
+          pisteet: joukkue.pisteet,
+          rastileimaukset: joukkue.rastileimaukset,
+          sarja: joukkue.sarja,   
+  };
+  joukkueTaulukko.push(joukkueAttr);      
+  }
+  console.log(joukkueTaulukko);
+}
+
+
+}
+
+
+
 
 window.addEventListener("load", alustus);
