188e2b30d6a4a752ae5d74044aa55cc66fd5ba0a
Kimi Haukka <haukka.kimi@gmail.com>
2022-11-18 21:25:36 +0200

    created terrain map

--- viikkotehtava5/vt5.js
+++ viikkotehtava5/vt5.js
@@ -13,3 +13,46 @@ window.addEventListener("load", function(e) {
 	    });
 });
 
+/* Luodaan map */
+/* let map = L.map('map').setView([62.232115, 25.735216], 10);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map); */
+
+
+let map = new L.map('map', {
+	crs: L.TileLayer.MML.get3067Proj()
+   }).setView([62.2333, 25.7333], 11);
+L.tileLayer.mml_wmts({ layer: "maastokartta", key : "3ad2a499-581c-4212-92d4-b1342b7a366d" }).addTo(map);
+
+
+/* Markkeri */
+let marker = L.marker([51.5, -0.09]).addTo(map);
+
+/* Circle */
+let circle = L.circle([51.508, -0.11], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(map);
+
+/* Polygon */
+let polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(map);
+
+/* popup */
+let popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
