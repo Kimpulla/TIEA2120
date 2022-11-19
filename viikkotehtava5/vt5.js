783fd2eaf0529dd2b714fa4cae0dc5935dddf13a
Kimi Haukka <haukka.kimi@gmail.com>
2022-11-19 18:25:32 +0200

    Created list for teams and started to create list for check marks.

--- viikkotehtava5/vt5.js
+++ viikkotehtava5/vt5.js
@@ -8,51 +8,99 @@ window.addEventListener("load", function(e) {
 	    .then(response => response.json())
 	    .then(function(data) {
             console.log(data);
-            // tänne oma koodi
+            
+
+
+/* Määritetään taulukoita */
+let joukkueTaulukko = [];
+
+/* Kutsutaan metodeja */
+joukkueetDeep();
+createTeamList();
+
+/* Luodaan maastokartta */
+let map = new L.map('map', {
+crs: L.TileLayer.MML.get3067Proj()
+}).setView([62.2333, 25.7333], 11);
+L.tileLayer.mml_wmts({ layer: "maastokartta", key : "3ad2a499-581c-4212-92d4-b1342b7a366d" }).addTo(map);
+            
+
+
+/**
+* Funktiolla luodaan lista joukkueista.
+*/
+function createTeamList(){ 
+
+/* Ensimmäinen forms -elementti. */
+    let form = document.forms[0];
+                        
+    /* Luodaan ul -elementti ja järjestetään taulukko*/
+    let ul = document.createElement("ul");
+    joukkueTaulukko.sort(compareJoukkue);
+            
+    /* Käydään joukkueet läpi ja lisätään listaukseen */
+    for(let i = 0; i < joukkueTaulukko.length;i++){
+              
+        /* Luodaan li -elementti */
+        let li = document.createElement("li");
+        li.appendChild(document.createTextNode(joukkueTaulukko[i]["nimi"]));
+        ul.appendChild(li);
+    }
+    form.appendChild(ul);
+}
+
+/**
+ * Funktiolla luodaan lista rasteista.
+ */
+function createRastitList(){
+
+}
+            
+/**
+* Funktiolla luodaan deepcopy taulukko alkuperäisestä data.joukkueet taulukosta.
+*/
+function joukkueetDeep(){
+            
+    let joukkueet = data.joukkueet;
+    for (let joukkue of joukkueet) {
+        let joukkueAttr = {
+        aika: joukkue.aika,
+        jasenet: joukkue.jasenet,
+        leimaustapa: joukkue.leimaustapa,
+        matka: joukkue.matka,
+        nimi: joukkue.nimi,
+        pisteet: joukkue.pisteet,
+        rastileimaukset: joukkue.rastileimaukset,
+        sarja: joukkue.sarja,   
+        };
+    	joukkueTaulukko.push(joukkueAttr);      
+    }
+    console.log("JoukkueTaulukko: ");
+     console.log(joukkueTaulukko);
+}
+            
+            
+/** 
+* Apufunktio, joka vertailee joukkeuiden nimiä.
+*
+* @param {String} joukkue1
+* @param {String} joukkue2
+* @returns -1, 0 tai 1
+*/
+function compareJoukkue(joukkue1, joukkue2) {
+            
+    if (joukkue1.nimi.toLowerCase() < joukkue2.nimi.toLowerCase()) {
+        return -1;
+    }
+    if (joukkue1.nimi.toLowerCase() > joukkue2.nimi.toLowerCase()) {
+        return 1;
+    }
+    	return 0;
+}
+            
+            
+
+
 
 	    });
 });
-
-/* Luodaan map */
-/* let map = L.map('map').setView([62.232115, 25.735216], 10);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map); */
-
-
-let map = new L.map('map', {
-	crs: L.TileLayer.MML.get3067Proj()
-   }).setView([62.2333, 25.7333], 11);
-L.tileLayer.mml_wmts({ layer: "maastokartta", key : "3ad2a499-581c-4212-92d4-b1342b7a366d" }).addTo(map);
-
-
-/* Markkeri */
-let marker = L.marker([51.5, -0.09]).addTo(map);
-
-/* Circle */
-let circle = L.circle([51.508, -0.11], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500
-}).addTo(map);
-
-/* Polygon */
-let polygon = L.polygon([
-    [51.509, -0.08],
-    [51.503, -0.06],
-    [51.51, -0.047]
-]).addTo(map);
-
-/* popup */
-let popup = L.popup();
-
-function onMapClick(e) {
-    popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(map);
-}
-
-map.on('click', onMapClick);
